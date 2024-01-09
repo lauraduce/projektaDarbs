1242b8647b0e018e3026a0ddca2b1a34f179df32
lauraduce <laura.duce_1@edu.rtu.lv>
2024-01-09 15:07:39 +0200

    Update projektaDarbs.py

--- projektaDarbs.py
+++ projektaDarbs.py
@@ -6,12 +6,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
+import re
 from openpyxl import Workbook, load_workbook
 print("Ievadiet valamas lietas nosaukumu un kvalitati:")
 item= input()
+
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
+
+DMarket=[]
 urlDM = "https://dmarket.com/ingame-items/item-list/csgo-skins"
 driver.get(urlDM)
 time.sleep(2)
@@ -22,6 +26,34 @@ time.sleep(2)
 
 
 priceFindDM = driver.find_element(By.CLASS_NAME, "c-asset__priceNumber")
-priceDM = priceFindDM.get_attribute('innerText')
-
+priceGetDM = priceFindDM.get_attribute('innerText')
+priceDM = float(re.sub('[^0-9.]', '', priceGetDM))
 print(priceDM)
+
+urlValutasKurss = "https://www.bank.lv/statistika/dati-statistika/valutu-kursi/aktualie"
+driver.get(urlValutasKurss)
+time.sleep(2)
+acceptCookies1 = driver.find_element(By.CLASS_NAME, "status")
+acceptCookies1.click()
+time.sleep(2)
+exchangeRateFind = driver.find_element(By.XPATH, '//*[@id="period2024-01-08"]/div[2]/div/div/div/table/tbody/tr[29]/td[3]')
+time.sleep(4)
+exchageRateGet = exchangeRateFind.get_attribute('innerText')
+exchangeRate = float(exchageRateGet)
+
+commissionDm = "3.85% + 0.27€"
+
+withoutCommissionDM = round((priceDM/exchangeRate), 2)
+withCommissionDM = round(((withoutCommissionDM)*1.0385 + 0.27), 2)
+
+minDM = 1/exchangeRate
+maxDM = 500/exchangeRate
+
+if minDM<=withoutCommissionDM<=maxDM:
+    limitsDM = "+"
+else:
+    limitsDM = "-"
+
+DMarket.extend([item, withoutCommissionDM, commissionDm, withCommissionDM, limitsDM])
+
+print(DMarket)
